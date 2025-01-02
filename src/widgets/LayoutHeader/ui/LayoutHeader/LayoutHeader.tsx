6931fdca1a2a959f8e4b57ff1e99661d2ec174ab
mick-olka <mickolka64@gmail.com>
2025-01-02 17:28:55 +0000

    upd: added map link to header layout & features text small change

--- src/widgets/LayoutHeader/ui/LayoutHeader/LayoutHeader.tsx
+++ src/widgets/LayoutHeader/ui/LayoutHeader/LayoutHeader.tsx
@@ -19,7 +19,7 @@ const LayoutHeader: FC = () => {
           <Logo logoName={"no boobs"} />
           <div className="navbar-end gap-6 hidden sm:flex">
             <button
-              className="w-fit btn-ghost btn-circle btn p-2"
+              className="w-36 btn-ghost btn-circle btn p-2"
               onClick={() => navigate("/rules")}
             >
               <svg
@@ -39,7 +39,27 @@ const LayoutHeader: FC = () => {
               Правила
             </button>
             <button
-              className="w-fit btn-ghost btn-circle btn p-2 mr-6"
+              className="w-36 btn-ghost btn-circle btn p-2"
+              onClick={() => navigate("/features")}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
+                />
+              </svg>
+              Карта
+            </button>
+            <button
+              className="w-36 btn-ghost btn-circle btn p-2 mr-6"
               onClick={() => navigate("/features")}
             >
               <svg
