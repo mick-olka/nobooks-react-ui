5d96ec2d7301166441a8ce6480ac067453545b91
mick-olka <mickolka64@gmail.com>
2025-01-16 12:44:58 +0000

    upd: bedrock ip

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 // export const MAP_URL = "https://sexy.wither.host/#world";
 export const MAP_URL = "https://sexy.mcpe.help/#world";
 export const JAVA_IP = "mc.noboobs.world";
-export const BEDROCK_IP = "bedrock.noboobs.world:19511";
+export const BEDROCK_IP = "bedrock.noboobs.world:25624";
