d549e69cae3a1ccaeab1b4ec076acc1aa22a2d6b
mick-olka <mickolka64@gmail.com>
2025-01-28 23:16:34 +0000

    fix: menu list navigation on mobile

--- src/widgets/LayoutHeader/ui/Menu/Menu.tsx
+++ src/widgets/LayoutHeader/ui/Menu/Menu.tsx
@@ -1,14 +1,15 @@
 import type { FC } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import type { Menu as MenuType } from "../../model/types";
 
 const Menu: FC<MenuType> = ({ links }: MenuType) => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="navbar-start dropdown">
         <div
           tabIndex={0}
-          // biome-ignore lint/a11y/useSemanticElements: <explanation>
+          // biome-ignore lint/a11y/useSemanticElements: <We can't use <button> here because Safari has a bug that prevents the button from being focused. <div role="button" tabindex="0"> is a workaround for this bug.>
           role="button"
           className="btn m-1 btn-ghost btn-circle"
         >
@@ -30,7 +31,7 @@ const Menu: FC<MenuType> = ({ links }: MenuType) => {
         </div>
         <ul className="dropdown-content menu menu-sm bg-base-100 rounded-box z-[50] w-52 p-2 shadow">
           {links.map((link) => (
-            <li key={link.name}>
+            <li key={link.name} onClick={() => navigate(link.href)}>
               <Link to={link.href}>{link.name}</Link>
             </li>
           ))}
