0a178fe5198144e89d9104614aafe2b37c71ab90
mick-olka <mickolka64@gmail.com>
2025-01-28 17:38:18 +0000

    feat: anchorlink implementation for features page

--- src/pages/Features/ui/Page.tsx
+++ src/pages/Features/ui/Page.tsx
@@ -1,6 +1,6 @@
 import { useFeaturesList } from "@/utils";
 import { PageTransitionWrapper } from "@/widgets";
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
 import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import beer from "../data/beer.md?raw";
@@ -12,45 +12,72 @@ import voicechat from "../data/voicechat.md?raw";
 const featuresData: string[] = [socialCredits, regions, voicechat, beer, other];
 
 const FeaturesPage: FC = () => {
-	const features = useFeaturesList(featuresData);
-	return (
-		<PageTransitionWrapper className="container mx-auto px-4 py-8">
-			<h1 className="text-4xl font-bold mb-12">
-				no boobs Зимовий - Особливості серверу
-			</h1>
-			{features.map((section) => (
-				<div key={section.title} className="mb-8">
-					<details className="cursor-pointer group">
-						<summary className="text-2xl font-semibold mb-4 list-none">
-							<span className="flex items-center">
-								<svg
-									className="w-6 h-6 mr-2 transition-transform duration-300 group-open:rotate-90"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<title>Arrow icon</title>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="2"
-										d="M9 5l7 7-7 7"
-									/>
-								</svg>
-								<Markdown className="markdown">{section.title}</Markdown>
-							</span>
-						</summary>
-						<div className="mt-2 pl-4 overflow-hidden transition-all duration-300 max-h-0 group-open:max-h-[1600px]">
-							<Markdown className="markdown" rehypePlugins={[rehypeRaw]}>
-								{section.content}
-							</Markdown>
-						</div>
-					</details>
-				</div>
-			))}
-		</PageTransitionWrapper>
-	);
+  const features = useFeaturesList(featuresData);
+  const hash = decodeURIComponent(window.location.hash.substring(1)).trim();
+  const setHash = (val: string) => {
+    window.location.hash = val;
+  };
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    if (hash) {
+      const element = document.getElementById(hash);
+      if (element) {
+        const details = element.querySelector("details");
+        console.log(details);
+        if (details) {
+          details.open = true; // Open the details element
+        }
+      }
+    }
+  }, []);
+
+  return (
+    <PageTransitionWrapper className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-12">
+        no boobs Зимовий - Особливості серверу
+      </h1>
+      {features.map((section) => (
+        <div key={section.id} className="mb-8" id={section.id}>
+          <details className="cursor-pointer group" open={section.id === hash}>
+            <summary
+              className="text-2xl font-semibold mb-4 list-none"
+              onClick={() => setHash(section.id)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                  setHash(section.id);
+                }
+              }}
+              tabIndex={0}
+            >
+              <span className="flex items-center">
+                <svg
+                  className="w-6 h-6 mr-2 transition-transform duration-300 group-open:rotate-90"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Arrow icon</title>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+                <Markdown className="markdown">{section.title}</Markdown>
+              </span>
+            </summary>
+            <div className="mt-2 pl-4 overflow-hidden transition-all duration-300 max-h-0 group-open:max-h-[1600px]">
+              <Markdown className="markdown" rehypePlugins={[rehypeRaw]}>
+                {section.content}
+              </Markdown>
+            </div>
+          </details>
+        </div>
+      ))}
+    </PageTransitionWrapper>
+  );
 };
 
 export default FeaturesPage;
--- src/utils/hooks/useFeaturesList.ts
+++ src/utils/hooks/useFeaturesList.ts
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
 
 type Feature = {
-	title: string;
-	content: string;
+  title: string;
+  content: string;
+  id: string;
 };
 
 export const useFeaturesList = (mdContentList: string[]) => {
-	const [features, setFeatures] = useState<Feature[]>([]);
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		const parsedFeatures = mdContentList
-			.map((content) => {
-				const data = content.split("|");
-				if (data?.length) {
-					return { title: data[0], content: data[1] };
-				}
-				return null;
-			})
-			.filter((result): result is Feature => result !== null);
+  const [features, setFeatures] = useState<Feature[]>([]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    const parsedFeatures = mdContentList
+      .map((content) => {
+        const data = content.split("|");
+        if (data?.length) {
+          return {
+            id: data[0].trim(),
+            title: data[1],
+            content: data[2],
+          };
+        }
+        return null;
+      })
+      .filter((result): result is Feature => result !== null);
 
-		setFeatures(parsedFeatures);
-	}, []);
-	return features;
+    setFeatures(parsedFeatures);
+  }, []);
+  return features;
 };
