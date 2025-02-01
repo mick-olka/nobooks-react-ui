245279dbf5d846a19e01ba276f7562101711d4d9
mick-olka <mickolka64@gmail.com>
2025-02-01 15:14:12 +0000

    upd: start with code text

--- src/pages/Start/ui/JavaStart.tsx
+++ src/pages/Start/ui/JavaStart.tsx
@@ -2,38 +2,38 @@ import { JAVA_IP } from "@/utils";
 import { IpContainer, PageTransitionWrapper } from "@/widgets";
 
 export const JavaStart = () => {
-	return (
-		<PageTransitionWrapper className="mb-8">
-			<div className="cursor-pointer group">
-				<div className="text-2xl font-semibold mb-4 list-none">
-					<span className="flex items-center">Грати з Java</span>
-				</div>
-				<div className="mt-2 pl-4 overflow-hidden transition-all duration-300 text-lg leading-relaxed">
-					<p className="w-fit">
-						Айпі для джави: <br /> <IpContainer ip={JAVA_IP} />
-					</p>
-					<p className="font-bold">Версія: (1.21+)</p>
-					<p>Якщо айпі не працює - спробуйте айпі бедроку</p>
-					<p>Проходка безплатна!</p>
-					<p>
-						Для того щоб розпочати Вашу гру на сервері потрібно зробити декілька
-						простих кроків:
-					</p>
-					<p>1. Зайти на майнкрафт сервер та отримати 4-х значний код</p>
-					<p>
-						{
-							"2. Відправте повідомлення /linkaccount <код> в канал #для-бота який знаходиться в нашому "
-						}
-						<u>
-							<a href="https://discord.com/invite/JKFY4tMhuA">Discord</a>
-						</u>
-					</p>
-					<p>3. Ви успішно зв'язали аккаунт і можете грати!</p>
-					<p>
-						При виникненні проблем/зміни ніку, звертайтесь до адміністрації.
-					</p>
-				</div>
-			</div>
-		</PageTransitionWrapper>
-	);
+  return (
+    <PageTransitionWrapper className="mb-8">
+      <div className="cursor-pointer group">
+        <div className="text-2xl font-semibold mb-4 list-none">
+          <span className="flex items-center">Грати з Java</span>
+        </div>
+        <div className="mt-2 pl-4 overflow-hidden transition-all duration-300 text-lg leading-relaxed">
+          <p className="w-fit">
+            Айпі для джави: <br /> <IpContainer ip={JAVA_IP} />
+          </p>
+          <p className="font-bold">Версія: (1.21+)</p>
+          <p>Якщо айпі не працює - спробуйте айпі бедроку</p>
+          <p>Проходка безплатна!</p>
+          <p>
+            Для того щоб розпочати Вашу гру на сервері потрібно зробити декілька
+            простих кроків:
+          </p>
+          <p>1. Зайти на майнкрафт сервер та отримати 4-х значний код</p>
+          <p>
+            {
+              "2. Відправте повідомлення /linkaccount code:<код> (або через підказку бота) в канал #для-бота який знаходиться в нашому "
+            }
+            <u>
+              <a href="https://discord.com/invite/JKFY4tMhuA">Discord</a>
+            </u>
+          </p>
+          <p>3. Ви успішно зв'язали аккаунт і можете грати!</p>
+          <p>
+            При виникненні проблем/зміни ніку, звертайтесь до адміністрації.
+          </p>
+        </div>
+      </div>
+    </PageTransitionWrapper>
+  );
 };
