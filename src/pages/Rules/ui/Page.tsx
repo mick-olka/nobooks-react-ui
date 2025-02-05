c8e07b798e47403a7257dbfb92508c9b383e531e
mick-olka <mickolka64@gmail.com>
2025-02-05 11:35:06 +0000

    upd: new global font & code linting

--- src/pages/Rules/ui/Page.tsx
+++ src/pages/Rules/ui/Page.tsx
@@ -3,21 +3,21 @@ import type { FC } from "react";
 import { rulesData } from "../data";
 
 const RulesPage: FC = () => {
-	return (
-		<PageTransitionWrapper className="container mx-auto px-4 py-8">
-			<h1 className="text-4xl font-bold mb-6">Правила та положення</h1>
-			{rulesData.map((section) => (
-				<div key={section.title} className="mb-8">
-					<h2 className="text-2xl font-semibold mb-4">{section.title}</h2>
-					<ul className="list-inside space-y-2">
-						{section.rules.map((rule) => (
-							<li key={rule}>{rule}</li>
-						))}
-					</ul>
-				</div>
-			))}
-		</PageTransitionWrapper>
-	);
+  return (
+    <PageTransitionWrapper className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-6">Правила та положення</h1>
+      {rulesData.map((section) => (
+        <div key={section.title} className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4">{section.title}</h2>
+          <ul className="list-inside space-y-2">
+            {section.rules.map((rule) => (
+              <li key={rule}>{rule}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </PageTransitionWrapper>
+  );
 };
 
 export default RulesPage;
--- src/widgets/Elements/ui/PageTransitionWrapper.tsx
+++ src/widgets/Elements/ui/PageTransitionWrapper.tsx
@@ -3,38 +3,38 @@ import { motion } from "framer-motion";
 import type { ReactNode } from "react";
 
 const pageVariants = {
-	initial: { opacity: 0, x: -0 },
-	in: { opacity: 1, x: 0 },
-	out: { opacity: 0, x: 0 },
+  initial: { opacity: 0, x: -0 },
+  in: { opacity: 1, x: 0 },
+  out: { opacity: 0, x: 0 },
 };
 
 type Props = {
-	children: ReactNode;
-	className?: string;
-	animate?: keyof typeof pageVariants;
-	initial?: keyof typeof pageVariants;
-	exit?: keyof typeof pageVariants;
+  children: ReactNode;
+  className?: string;
+  animate?: keyof typeof pageVariants;
+  initial?: keyof typeof pageVariants;
+  exit?: keyof typeof pageVariants;
 };
 
 const PageTransitionWrapper = ({
-	children,
-	className,
-	animate = "in",
-	initial = "initial",
-	exit = "out",
+  children,
+  className,
+  animate = "in",
+  initial = "initial",
+  exit = "out",
 }: Props) => {
-	return (
-		<motion.section
-			className={cn(className)}
-			initial={initial}
-			animate={animate}
-			exit={exit}
-			variants={pageVariants}
-			transition={{ duration: 0.5 }}
-		>
-			{children}
-		</motion.section>
-	);
+  return (
+    <motion.section
+      className={cn(className)}
+      initial={initial}
+      animate={animate}
+      exit={exit}
+      variants={pageVariants}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.section>
+  );
 };
 
 export default PageTransitionWrapper;
