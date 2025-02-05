5b8ef52b36596579aa0df3eee37fc5fdca94be72
mick-olka <mickolka64@gmail.com>
2025-02-05 16:07:49 +0000

    upd: map in progress & start playing text

--- src/pages/Map/ui/Page.tsx
+++ src/pages/Map/ui/Page.tsx
@@ -1,30 +1,27 @@
-import { MAP_URL } from "@/utils";
+// import { MAP_URL } from "@/utils";
 import { DefaultLoader, PageTransitionWrapper } from "@/widgets";
 import { type FC, Suspense } from "react";
+import { Navigate } from "react-router-dom";
 
 const MapPage: FC = () => {
-	// const [isError, setIsError] = useState(false);
-	return (
-		<>
-			<Suspense fallback={<DefaultLoader />}>
-				<PageTransitionWrapper>
-					<div className="hero min-h-[calc(100vh-64px)] bg-black">
-						<iframe
-							height="100%"
-							id="no-boobs-map"
-							src={MAP_URL}
-							title="no boobs map"
-							width="100%"
-							// onError={() => {
-							//   alert("error");
-							//   setIsError(true);
-							// }}
-						/>
-					</div>
-				</PageTransitionWrapper>
-			</Suspense>
-		</>
-	);
+  return (
+    <>
+      <Suspense fallback={<DefaultLoader />}>
+        <PageTransitionWrapper>
+          {/* <div className="hero min-h-[calc(100vh-64px)] bg-black">
+            <iframe
+              height="100%"
+              id="no-boobs-map"
+              src={MAP_URL}
+              title="no boobs map"
+              width="100%"
+            />
+          </div> */}
+          <Navigate to={"/in-progress"} />
+        </PageTransitionWrapper>
+      </Suspense>
+    </>
+  );
 };
 
 export default MapPage;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
-// export const MAP_URL = "https://sexy.wither.host/#world";
+// export const MAP_URL =
+//   "https://map.noboobs.world:45609/#world:-64:0:128:1500:0:0:0:0:perspective";
 export const MAP_URL = "https://sexy.mcpe.help/#world";
 export const JAVA_IP = "mc.noboobs.world";
 export const BEDROCK_IP = "bedrock.noboobs.world:25624";
